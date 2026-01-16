5c36cce2c6826e59717ab5f5cf495f842b36b0bc
zoe waters <zowa22@student.bth.se>
2026-01-16 11:12:46 +0100

    mobilanpassat

--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -1,9 +1,11 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { logout } from '../../authUtils';
 import "../../style/sidebar.css";
 
 export default function Sidebar() {
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
 
     const handleLogout = () => {
         logout();
@@ -11,23 +13,24 @@ export default function Sidebar() {
     };
 
     return (
-        <div className="sidebar">
+        <nav className={"sidebar" + (open ? " open" : "") }>
             <div className="logo">
                 <img
-                src={process.env.PUBLIC_URL + "/logga.png"}
-                alt="VTeam logga"
-                className="logo-img"
+                    src={process.env.PUBLIC_URL + "/logga.png"}
+                    alt="VTeam logga"
+                    className="logo-img"
                 />
+                <button className="sidebar-toggle" onClick={() => setOpen(o => !o)}>
+                    <span className="hamburger">&#9776;</span>
+                </button>
             </div>
-            <Link to="/">Home</Link>
-            <br />
-            <Link to="/maps">Go to Bikes</Link>
-            <br />
-            <Link to="/profile">Go to Profile</Link>
-            <br />
-            <Link to="/payments">Betalningar</Link>
-            <br />
-            <button onClick={handleLogout} className="logout-btn">Logout</button>
-        </div>
+            <div className="sidebar-links">
+                <Link to="/">Home</Link>
+                <Link to="/maps">Go to Bikes</Link>
+                <Link to="/profile">Go to Profile</Link>
+                <Link to="/payments">Betalningar</Link>
+                <button onClick={handleLogout} className="logout-btn">Logout</button>
+            </div>
+        </nav>
     );
 }
